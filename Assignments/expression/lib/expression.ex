25fd7b00ddd6db2e444ef3b681114b729a6e9cf6
William Fridh <william.fridh@gmail.com>
2024-01-29 20:55:28 +0100

    stuff

--- Assignments/expression/lib/expression.ex
+++ Assignments/expression/lib/expression.ex
@@ -1,4 +1,5 @@
 defmodule Expression do
+
   @moduledoc """
   Documentation for `Expression`.
 
@@ -21,8 +22,13 @@ defmodule Expression do
     | {:div, expr(), expr()}
     | literal()
 
+
+
   @doc """
-  Hello world.
+  # Evaluate
+
+  Evaluates a given expression based on the provided environment
+  holding the variable values.
 
   ## Examples
 
@@ -32,11 +38,55 @@ defmodule Expression do
   """
   def eval({:num, n}, _) do n end
   def eval({:var, v}, env) do Env.lookup(env, v) end
+  def eval({:q, v1, v2}, _) do simplify_q({:q, v1, v2}) end
 
-  def eval(q, _) do
-    simplify_q(q)
+  def eval({:add, e1, e2}, env) do add(eval(e1, env), eval(e2, env)) end
+  def eval({:sub, e1, e2}, env) do sub(eval(e1, env), eval(e2, env)) end
+  def eval({:mul, e1, e2}, env) do mul(eval(e1, env), eval(e2, env)) end
+  def eval({:div, e1, e2}, env) do {:div, eval(e1, env), eval(e2, env)} end
+
+
+  @doc"""
+  # Addition
+  """
+  def add(v1, v2) do v1 + v2 end
+  def add(e1, e2) do
+    case [e1, e2] do
+      [{:q, v1, v2}, {:q, v3, v4}] -> {:q, v1*v4+v3*v2, v2*v4}
+      [{:q, v1, v2}, {:num, n1}] -> {:q, v1+n1*v2, v2}
+      [{:num, n1}, {:q, v1, v2}] -> {:q, v1+n1*v2, v2}
+    end
   end
 
+
+  @doc"""
+  # Subtraction
+  """
+  def sub(v1, v2) do v1 - v2 end
+
+
+  @doc"""
+  # Multiplication
+  """
+  def mul(v1, v2) do v1 * v2 end
+
+
+  @doc"""
+  # Division
+  """
+  def division(v1, v2) do {:q, v1, v2} end
+
+
+
+  @doc """
+  # Simplify Rational Number
+
+  This function is used by eval().
+
+  ## Example
+    iex> Expression.simplify_q({:q, 16, 32})
+    {:q, 1.0, 2.0}
+  """
   def simplify_q({:q, v1, v2}) do simplify_q({:q, v1, v2}, v2) end
   def simplify_q({:q, v1, v2}, d) do
     if rem(v1, d) == 0 and rem(v2, d) == 0 do
@@ -50,33 +100,4 @@ defmodule Expression do
     end
   end
 
-  def eval({:add, e1, e2}, env) do
-    add(eval(e1, env), eval(e2, env))
-  end
-
-  #def eval({:add, e1, e2}, env) do
-  #  case {elem(e1, 0), elem(e2, 0)} do
-  #    {:q, :q} ->
-  #    {:q, _} -> {:q, }
-  #    {_, :q} ->
-  #    _ ->
-  #end
-
-
-
-
-  def eval({:sub, e1, e2}, env) do
-    sub(eval(e1, env), eval(e2, env))
-  end
-  def eval({:mul, e1, e2}, env) do
-    mul(eval(e1, env), eval(e2, env))
-  end
-  def eval({:div, e1, e2}, env) do
-    {:div, eval(e1, env), eval(e2, env)}
-  end
-
-  def add(v1, v2) do v1 + v2 end
-  def sub(v1, v2) do v1 - v2 end
-  def mul(v1, v2) do v1 * v2 end
-  def division(v1, v2) do {:q, v1, v2} end
 end
